616a64e25ce27d3a83b2ed1c14a52ae38dc33a40
Franciele Tadiello <franciele.p.t@gmail.com>
2023-08-13 10:50:11 -0300

    Escolhendo taxa de pagamento

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,6 +1,19 @@
 import cardapio from "./model/Cardapio";
 
 class CaixaDaLanchonete {
+    calcularTaxa (metodoDePagamento) {
+        switch(metodoDePagamento){
+            case "dinheiro":
+                return 0.95
+                break
+            case "credito":
+                return 1.03
+                break
+            case "debito":
+                return 1
+                break
+        }
+    }
 
     calcularValorDaCompra(metodoDePagamento, itens) {
         return "";
