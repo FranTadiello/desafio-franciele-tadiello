d073c2346bb1d92ccca39ba080adccda0a4b9daa
Franciele Tadiello <franciele.p.t@gmail.com>
2023-08-13 12:44:13 -0300

    Tratando item invalido e forma de pagamento

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -12,33 +12,54 @@ class CaixaDaLanchonete {
             case "debito":
                 return 1
                 break
+            default:
+                throw new Error("Erro de metodo de pagamento");   
         }
     }
     calcularValorDosItens(itens){
         let somaValores = 0
         itens.forEach(item => {
             item = item.split(",")
-            const produto = item[0]
+            
+            let produto = item[0]            
+            produto = cardapio[produto]
+
+            if(item.length < 2 || produto === undefined){
+                throw new Error("Erro de entrada de itens");
+            }
+            
             const qtd = item[1]
+
             if(qtd == 0){
                 throw new Error("Erro de quantidade");
             }
-            somaValores += cardapio[produto].valor*qtd 
+
+            somaValores += produto.valor*qtd 
         })
         return somaValores
     }
+
     calcularValorDaCompra(metodoDePagamento, itens) {
         let valorBruto
+        let taxa
         if(itens.length === 0){
             return "Não há itens no carrinho de compra!"
         }
         try {
-            valorBruto = this.calcularValorDosItens(itens)    
+            valorBruto = this.calcularValorDosItens(itens)
+            taxa = this.calcularTaxa(metodoDePagamento)
         } catch (error) {
-            return 'Quantidade inválida!'
+            if(error.message === "Erro de quantidade"){
+                return 'Quantidade inválida!'
+            }
+            if(error.message === "Erro de entrada de itens"){
+                return 'Item inválido!'
+            }
+            if(error.message === "Erro de metodo de pagamento"){
+                return 'Forma de pagamento inválida!'
+            }
         }
-
-        const taxa = this.calcularTaxa(metodoDePagamento)
+        
         return `R$ ${((valorBruto*taxa).toFixed(2)).replace(".",",")}`;
     }
 
